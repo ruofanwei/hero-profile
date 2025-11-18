850ff623bc5a32b8b58f4c8da7c48e35897d8990
Ruofan Wei <px732amgo81sp8@gmail.com>
2025-11-18 19:00:20 +0800

    chore: bootstrap Vite React project

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-styled-components'],
+      },
+    }),
+  ],
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+  },
+  preview: {
+    host: '127.0.0.1',
+    port: 5173,
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    globals: true,
+  },
+});
